db50eae990c8867e83db8617d0e04ee194ecc28b
Harsh_Dubey <harshaf6969@gmail.com>
2025-12-15 21:41:15 +0530

    Delete Backend/routes directory

--- Backend/routes/host_Routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-const hostRoutes = express.Router()
-
-hostRoutes.get("/add-home",(req,res)=>{
-    res.sendFile()
-})
-
-module.exports = hostRoutes
